0ae85a140fb239c5672e2c6c9cfa12478a8ac5ff
nav <navraj213@gmail.com>
2024-11-09 00:20:53 -0500

    Create framework for branches

---
+++ src/app/student/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <div className="flex flex-col min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+          <h1 className="text-5xl font-extrabold text-center py-2 animate-slideIn">AttendMate</h1>
+          <p className="text-5xl font-extrabold text-center py-2 animate-slideIn" >STUDENT LOGIN</p>
+      </div>
+    );
+
+    // connect api and get student data and display it here
+  }
+  
